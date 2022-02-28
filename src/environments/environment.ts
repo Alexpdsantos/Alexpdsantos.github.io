5f30e26c31a4392f1907e9a4f3ca41fbce4f9e65
Alex Santos <amelo3433@gmail.com>
2022-02-28 00:07:15 -0300

    Ajustes no Front-End

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
+  production: false,
   baseUrl: 'https://curso-ordens-de-servico.herokuapp.com/'
 };
 
